e9177301e515b757d27667632a30ad6a9f613682
bo <q463250938@gmail.com>
2024-07-08 15:24:30 -0700

    Initial Commit

---
+++ SwiftUI-Custom-Components/SwiftUI_Custom_ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Custom_ComponentsApp.swift
+//  SwiftUI-Custom-Components
+//
+//  Created by Bo Zhong on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Custom_ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
